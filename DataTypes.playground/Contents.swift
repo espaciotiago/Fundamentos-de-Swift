1678313e2fad391973bb9f5fddf2cd22843bad18
Santiago Moreno <msantim@hotmail.com>
2024-10-17 14:12:59 -0500

    DataTypes

---
+++ DataTypes.playground/Contents.swift
@@ -0,0 +1,14 @@
+
+// Esto es un comentario. No se ejecutará como código.
+
+// Número entero
+let numberInt = 10
+
+// Número con decimales
+let numberDouble = 10.0
+
+// Cadena de caracteres
+let string = "Hello, World!"
+
+// Valor booleano (verdadero o flaso)
+let bool = true
