13874e6990902a675bbd3f4d12b576311c19350b
Santiago Moreno <msantim@hotmail.com>
2024-10-17 14:27:36 -0500

    Variables

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,51 @@
+// Contante
+let constant = "I am a constant"
+
+// Variable
+var variable = "Hello, playground"
+print(variable)
+
+// Modificar una variable
+variable = "World"
+print(variable)
+
+// MARK: - Operadores númericos (Enteros)
+
+let firstNumber = 10
+let secondNumber = 20
+
+// Suma
+let sum = firstNumber + secondNumber
+
+// Resta
+let difference = firstNumber - secondNumber
+
+// Multiplicación
+let product = firstNumber * secondNumber
+
+// División
+let quotient = firstNumber / secondNumber
+
+// MARK: - Operadores númericos (Con decimales)
+let firstDecimalNumber = 10.0
+let secondDecimalNumber = 20.0
+
+// División con decimales
+let quotientDecimal = firstDecimalNumber / secondDecimalNumber
+
+// MARK: - Operadores de strings
+let firstString = "Hello"
+let secondString = "World"
+let concatenatedString = firstString + " " + secondString
+
+let name = "Tiago"
+let greeting = "Hello, \(name)"
+
+let newString = "Number of characters: \(concatenatedString.count)"
+
+// MARK: - Operadores booleanos
+let firstBool = true
+let secondBool = false
+
+let orResult = firstBool || secondBool
+let andResult = firstBool && secondBool
