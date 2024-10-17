27fa65fca808090947d520788a107c83cca43b6c
Santiago Moreno <msantim@hotmail.com>
2024-10-17 14:29:19 -0500

    Types added

--- DataTypes.playground/Contents.swift
+++ DataTypes.playground/Contents.swift
@@ -3,12 +3,16 @@
 
 // Número entero
 let numberInt = 10
+let newNumberInt: Int = 10
 
 // Número con decimales
 let numberDouble = 10.0
+let newNumberDouble: Double = 10
 
 // Cadena de caracteres
 let string = "Hello, World!"
+let newString: String = "Hello, World!"
 
 // Valor booleano (verdadero o flaso)
 let bool = true
+let newBool: Bool = true
